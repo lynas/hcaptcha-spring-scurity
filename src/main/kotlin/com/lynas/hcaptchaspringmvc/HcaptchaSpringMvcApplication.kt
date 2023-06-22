7de8f7f6549ed48b9b3c63a5092c9a0de638838b
Sazzad Islam <sazzad.islam@reev.com>
2023-06-22 16:03:19 +0200

    custom security context :D

--- src/main/kotlin/com/lynas/hcaptchaspringmvc/HcaptchaSpringMvcApplication.kt
+++ src/main/kotlin/com/lynas/hcaptchaspringmvc/HcaptchaSpringMvcApplication.kt
@@ -10,16 +10,24 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
-//import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.authority.AuthorityUtils
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.crypto.bcrypt.BCrypt
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY
 import org.springframework.stereotype.Controller
 import org.springframework.util.LinkedMultiValueMap
 import org.springframework.util.MultiValueMap
+import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.client.RestTemplate
-import java.util.UUID
+import java.util.*
 import javax.persistence.*
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpSession
 
 
 @SpringBootApplication
@@ -27,18 +35,19 @@ class HcaptchaSpringMvcApplication {
 	@Bean
 	fun restTemplate(): RestTemplate = RestTemplateBuilder().build()
 
-//	@Bean
-//	fun runner(
-//		userRepository: UserRepository,
-//		passwordEncoder: PasswordEncoder
-//	) = CommandLineRunner {
-//		val user = AppUser().apply {
-//			id = UUID.randomUUID().toString()
-//			username = "admin"
-//			password = passwordEncoder.encode("admin")
-//		}
-//		userRepository.save(user)
-//	}
+	@Bean
+	fun runner(
+		userRepository: UserRepository,
+		passwordEncoder: PasswordEncoder
+	) = CommandLineRunner {
+		val user = AppUser().apply {
+			id = UUID.randomUUID().toString()
+			username = "admin"
+			password = passwordEncoder.encode("admin")
+		}
+		userRepository.save(user)
+	}
+
 }
 
 fun main(args: Array<String>) {
@@ -47,14 +56,15 @@ fun main(args: Array<String>) {
 
 @Controller
 class HomeController(
-	private val restTemplate: RestTemplate
+	private val restTemplate: RestTemplate,
+	private val userRepository: UserRepository
 ) {
 
 	@Value("\${hCaptcha.secret.key}")
 	private lateinit var hCaptchaSecretKey: String
 
 	@RequestMapping("/")
-	fun home(): String {
+	fun home(request: HttpServletRequest): String {
 		return "home"
 	}
 	@RequestMapping("/login")
@@ -63,19 +73,38 @@ class HomeController(
 	}
 
 
-	@PostMapping("/login")
-	fun loginPost(@RequestParam("h-captcha-response") captchaResponse: String): String {
+	@PostMapping("/loginUser")
+	fun loginPost(
+		@RequestParam("h-captcha-response") captchaResponse: String,
+		@ModelAttribute appUserDto: AppUserDto,
+		request: HttpServletRequest
+	): String {
 		val headers = HttpHeaders()
 		headers.contentType = MediaType.APPLICATION_FORM_URLENCODED
-		val map = LinkedMultiValueMap<String, String>()
-		map.add("response", captchaResponse)
-		map.add("secret", hCaptchaSecretKey)
-		val request = HttpEntity<MultiValueMap<String, String>>(map, headers)
+		val map = LinkedMultiValueMap<String, String>().apply {
+			add("response", captchaResponse)
+			add("secret", hCaptchaSecretKey)
+		}
+
+		val apiRequest = HttpEntity<MultiValueMap<String, String>>(map, headers)
 		val url = "https://hcaptcha.com/siteverify"
-		val response = restTemplate.postForEntity(url, request, HCaptchaResponse::class.java)
-		println("h-captcha verification response")
-		println(response.body?.success)
-		return "login"
+		val response = restTemplate.postForEntity(url, apiRequest, HCaptchaResponse::class.java)
+		if (response.body?.success == false) {
+			throw RuntimeException("invalid captcha")
+		}
+
+		val appUser = userRepository.findByUsername(appUserDto.username)
+		val isValidUser = checkPassword(appUserDto.password, appUser?.password ?: "")
+		if (!isValidUser) {
+			throw RuntimeException("invalid user or password")
+		}
+
+		val sc = SecurityContextHolder.getContext()
+		sc.authentication = UsernamePasswordAuthenticationToken("username", "password",
+			AuthorityUtils.createAuthorityList("ROLE_USER"))
+		val session: HttpSession = request.getSession(true)
+		session.setAttribute(SPRING_SECURITY_CONTEXT_KEY, sc)
+		return "redirect:/"
 	}
 }
 
@@ -90,6 +119,11 @@ class AppUser {
 	lateinit var password: String
 }
 
+data class AppUserDto(
+	val username: String,
+	val password: String
+)
+
 
 interface UserRepository : JpaRepository<AppUser, String> {
 	fun findByUsername(username: String): AppUser?
@@ -98,3 +132,5 @@ interface UserRepository : JpaRepository<AppUser, String> {
 data class HCaptchaResponse(
 	val success: Boolean
 )
+
+fun checkPassword(passwordClean: String, passwordHash: String): Boolean = BCrypt.checkpw(passwordClean, passwordHash)
